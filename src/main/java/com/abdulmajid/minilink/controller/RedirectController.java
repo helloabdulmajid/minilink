a9241434efafc897274e4ae9c14fe8da9487f30d
Abdul Majid <helloabdulmajid@gmail.com>
2026-05-19 03:40:03 +0530

    docs: enhance Swagger API documentation

--- src/main/java/com/abdulmajid/minilink/controller/RedirectController.java
+++ src/main/java/com/abdulmajid/minilink/controller/RedirectController.java
@@ -1,6 +1,9 @@
 package com.abdulmajid.minilink.controller;
 
 import com.abdulmajid.minilink.service.ShortUrlService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,11 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Redirect APIs", description = "APIs for URL redirection")
 public class RedirectController {
 
     private final ShortUrlService shortUrlService;
 
     @GetMapping("/{shortCode}")
+    @Operation(
+            summary = "Redirect To Original URL",
+            description = "Redirects user to the original long URL"
+    )
+    @ApiResponse(
+            responseCode = "302",
+            description = "Redirect successful"
+    )
     public RedirectView redirectToOriginalUrl(
             @PathVariable String shortCode
     ) {
--- src/main/java/com/abdulmajid/minilink/controller/ShortUrlController.java
+++ src/main/java/com/abdulmajid/minilink/controller/ShortUrlController.java
@@ -4,20 +4,33 @@ import com.abdulmajid.minilink.dto.CreateShortUrlRequest;
 import com.abdulmajid.minilink.dto.ShortUrlResponse;
 import com.abdulmajid.minilink.dto.UrlAnalyticsResponse;
 import com.abdulmajid.minilink.service.ShortUrlService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/api/v1/urls")
 @RequiredArgsConstructor
+
+@Tag(name = "Short URL APIs", description = "APIs for URL shortening and analytics")
 public class ShortUrlController {
 
     private final ShortUrlService shortUrlService;
 
     @PostMapping
+    @Operation(
+            summary = "Create Short URL",
+            description = "Creates a shortened URL with optional custom alias and expiration"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Short URL created successfully"
+    )
     public ResponseEntity<ShortUrlResponse> createShortUrl(
             @Valid @RequestBody CreateShortUrlRequest request
     ) {
@@ -31,6 +44,14 @@ public class ShortUrlController {
     }
 
     @GetMapping("/{shortCode}/analytics")
+    @Operation(
+            summary = "Get URL Analytics",
+            description = "Fetch analytics details including click count and expiration"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Analytics fetched successfully"
+    )
     public ResponseEntity<UrlAnalyticsResponse> getUrlAnalytics(
             @PathVariable String shortCode
     ) {
