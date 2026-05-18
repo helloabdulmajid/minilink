f6e5c244d22414934ddbb539c871f43f57ea5701
Abdul Majid <helloabdulmajid@gmail.com>
2026-05-19 01:12:08 +0530

    feat: implement URL creation service layer

---
+++ src/main/java/com/abdulmajid/minilink/service/ShortUrlService.java
@@ -0,0 +1,9 @@
+package com.abdulmajid.minilink.service;
+
+import com.abdulmajid.minilink.dto.CreateShortUrlRequest;
+import com.abdulmajid.minilink.dto.ShortUrlResponse;
+
+public interface ShortUrlService {
+
+    ShortUrlResponse createShortUrl(CreateShortUrlRequest request);
+}
---
+++ src/main/java/com/abdulmajid/minilink/service/impl/ShortUrlServiceImpl.java
@@ -0,0 +1,53 @@
+package com.abdulmajid.minilink.service.impl;
+
+import com.abdulmajid.minilink.dto.CreateShortUrlRequest;
+import com.abdulmajid.minilink.dto.ShortUrlResponse;
+import com.abdulmajid.minilink.entity.ShortUrl;
+import com.abdulmajid.minilink.repository.ShortUrlRepository;
+import com.abdulmajid.minilink.service.ShortUrlService;
+import com.abdulmajid.minilink.util.ShortCodeGenerator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ShortUrlServiceImpl implements ShortUrlService {
+
+    private final ShortUrlRepository shortUrlRepository;
+
+    @Override
+    public ShortUrlResponse createShortUrl(CreateShortUrlRequest request) {
+
+        String shortCode = generateUniqueShortCode();
+
+        ShortUrl shortUrl = ShortUrl.builder()
+                .originalUrl(request.getOriginalUrl())
+                .shortCode(shortCode)
+                .customAlias(request.getCustomAlias())
+                .build();
+
+        ShortUrl savedUrl = shortUrlRepository.save(shortUrl);
+
+        return ShortUrlResponse.builder()
+                .originalUrl(savedUrl.getOriginalUrl())
+                .shortCode(savedUrl.getShortCode())
+                .shortUrl("http://localhost:8080/" + savedUrl.getShortCode())
+                .customAlias(savedUrl.getCustomAlias())
+                .clickCount(savedUrl.getClickCount())
+                .createdAt(savedUrl.getCreatedAt())
+                .expiresAt(savedUrl.getExpiresAt())
+                .build();
+    }
+
+    private String generateUniqueShortCode() {
+
+        String shortCode;
+
+        do {
+            shortCode = ShortCodeGenerator.generateShortCode();
+        }
+        while (shortUrlRepository.existsByShortCode(shortCode));
+
+        return shortCode;
+    }
+}
