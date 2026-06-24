749b243c43fffcbdb6fde8e57634363b0d44ff61
Abdul Majid <helloabdulmajid@gmail.com>
2026-06-25 02:17:37 +0530

    nothing

--- src/main/java/com/abdulmajid/minilink/controller/HealthController.java
+++ src/main/java/com/abdulmajid/minilink/controller/HealthController.java
@@ -8,6 +8,6 @@ public class HealthController {
 
     @GetMapping("/health")
     public String health() {
-        return "MiniLink Backend Running";
+        return "MiniLink Backend Running ";
     }
 }
